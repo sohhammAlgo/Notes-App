d05a2fc4e654ef726d792c301b7aa4c290ccd76f
sohhammAlgo <guravsoham61@gmail.com>
2026-01-10 01:46:17 +0530

    Backend done

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,27 @@
 const express = require('express');
 const cors = require('cors');
-const {connectDB} = require('../config/db');
-const notesRoutes = require('../routes/notesRoutes');
-const dotenv = require('dotenv');
+const connectDB = require('./config/db'); 
+const notesRoutes = require('./routes/notesRoutes');
+require('dotenv').config();
 
-dotenv.config();
-connectDB();
 const app = express();
 
+// DB connection
+connectDB();
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/notes',notesRoutes);
+// Routes
+app.use('/api/notes', notesRoutes);
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('Server is running');
 });
 
+// Server
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT,(req,res)=>{
-    res.send(`Server is running on ${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
